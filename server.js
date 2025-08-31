94f1421915b99fe5c1d0dbb80254ba443480aa04
Mark-Cyber0628 <szakalmark0628@gmail.com>
2025-08-31 15:00:40 +0200

    elérési útvonal frissítve server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const bcrypt = require('bcrypt');
 const app = express();
 const PORT = 3000;
 
-app.use(express.static(path.join(__dirname,'public')));
+app.use(express.static(path.join(__dirname,)));
 app.use(express.json());
 
 // Projektek API
